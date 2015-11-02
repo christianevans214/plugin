a602e6256252f3a885b7890ef65451c9fad8d8c6
christianevans <christianevans@christianevans-macbookpro.roam.corp.google.com>
2015-11-01 21:21:49 -0800

    some navbar stylins

--- pre-build/javascripts/Navbar.js
+++ pre-build/javascripts/Navbar.js
@@ -6,11 +6,19 @@ var Navbar = React.createClass({
 			<div className = "nav-container">
 				<div className = "nav-flex nav-float-left"></div>
 				<div className = "nav-flex nav-center">
-					<div className = "nav-logo">
-						<h1>Plug In</h1>
-					</div>
+					<ul className ="nav-center-list">
+						<li className = "nav-center-list-item nav-link">Discover</li>
+						<li className = "nav-center-list-item nav-link">Account</li>
+						<li className = "nav-center-list-item">
+							<div className = "nav-logo">
+								<h1>Plug In</h1>
+							</div>
+						</li>
+						<li className = "nav-center-list-item nav-link">About</li>
+						<li className = "nav-center-list-item nav-link">Contact</li>
+					</ul>
 				</div>
-				<div className = "nav-flex nav-float-right"></div>
+				<div className = "nav-flex nav-float-right nav-link"></div>
 			</div>
 			)
 	}
